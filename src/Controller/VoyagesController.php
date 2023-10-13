64524b1960b20bed81e58ba5baefa42d297a785f
Paul FENOUILLET <paul.terraria@gmail.com>
2023-10-13 11:50:49 +0200

    Phase 9: ORM (tri)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -28,11 +28,24 @@ class VoyagesController extends AbstractController{
      * @return Response
      */
     public function index(): Response{
-        $visite = $this->repository->findAll();
+        $visites = $this->repository->findAllOrderBy('datecreation','DESC');
         return $this->render("pages/voyages.html.twig", [
-            'visites' => $visite
+            'visites' => $visites
         ]);
     }
+    /**
+     * @Route("/voyages/tri/{champ}/{ordre}", name="voyages.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @return Response
+     */
+    public function sort($champ, $ordre):Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
     
     public function __construct(VisiteRepository $repository) {
         $this->repository=$repository;
